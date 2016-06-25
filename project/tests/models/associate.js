9150d0f6292e0b2890f8addb992693394e0aa2f7
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-06-25 16:57:14 +0530

    adde

--- project/tests/models/associate.js
+++ project/tests/models/associate.js
@@ -12,7 +12,21 @@ describe('Associate Model', function () {
             expect(rows[0]).to.have.property('entity_id');
             console.log("Number of Associates = " + rows.length);
             for (var i = 0; i < rows.length; i++) {
-                console.log("id = " + rows[i].name + "element_id = " + rows[i].element_id + "entity_id = " + rows[i].entity_id + "\n");
+                console.log("id = " + rows[i].id + "; element_id = " + rows[i].element_id + "; entity_id = " + rows[i].entity_id + "\n");
+            }
+        });
+    });
+    
+    it('should get the Associates list for a particular element_id 1 and have properties id, element_id, entity_id', function () {
+        Associate.getByElement(1, function (err, rows) {
+            expect(err).to.equal(null);
+            expect(rows.length).to.be.above(0);
+            expect(rows[0]).to.have.property('id');
+            expect(rows[0]).to.have.property('element_id');
+            expect(rows[0]).to.have.property('entity_id');
+            console.log("Number of Associates for element_id 1 = " + rows.length);
+            for (var i = 0; i < rows.length; i++) {
+                console.log("id = " + rows[i].id + "; element_id = " + rows[i].element_id + "; entity_id = " + rows[i].entity_id + " for element_id = 1\n");
             }
         });
     });
