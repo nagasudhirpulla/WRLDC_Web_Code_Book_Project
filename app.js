3a28ee0dc607b1031b2e4a5306ef8fdc6bbd6689
nagasudhirpulla <nagasudhirpulla@gmail.com>
2016-07-01 10:22:45 +0530

    added favicon to the app

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var db = require('./project/db');
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-cors = require('./project/cors');
+var cors = require('./project/cors');
+var favicon = require('serve-favicon');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
@@ -19,6 +21,8 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 
 app.set('json spaces', 1);
+
+app.use(favicon(__dirname + '/project/public/img/favicon.ico'));
 
 app.use('/api/categories', require('./project/controllers/category'));
 app.use('/api/rldcs', require('./project/controllers/rldc'));
@@ -32,10 +36,10 @@ app.use('/', require('./project/controllers/general'));
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -43,23 +47,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 // Connect to MySQL on start
