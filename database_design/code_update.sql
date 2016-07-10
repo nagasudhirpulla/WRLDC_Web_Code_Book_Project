ab6632d2add6799d20ca6ab1e0c8563c9c60a2af
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2016-07-10 17:18:18 +0530

    SQL to update complete code through SQL transaction

--- database_design/code_update.sql
+++ database_design/code_update.sql
@@ -1,10 +1,19 @@
-UPDATE codes SET time=?,category_id=?,description=?,element_id=?,is_cancelled=? WHERE id=?
+START TRANSACTION READ WRITE;
 
-DELETE FROM code_requests WHERE code_id=?
-INSERT INTO code_requests(entity_id, code_id) VALUES (?,?)
+UPDATE codes SET time=?,category_id=?,description=?,element_id=?,is_cancelled=? WHERE id=?;
 
-DELETE FROM optional_codes WHERE code_id=?
-INSERT INTO optional_codes(code_id, rldc_id, code) VALUES (?,?,?)
+DELETE FROM code_requests WHERE code_id=?;
+INSERT INTO code_requests(entity_id, code_id) VALUES (?,?);
+
+DELETE FROM optional_codes WHERE code_id=?;
+INSERT INTO optional_codes(code_id, rldc_id, code) VALUES (?,?,?);
+
+DELETE FROM times WHERE code_id = 131;
+INSERT INTO times (code_id, time) VALUES (131, CURRENT_TIMESTAMP);
+
+COMMIT;
+
+
 
 START TRANSACTION READ WRITE;
 DELETE FROM times WHERE code_id = 131;
